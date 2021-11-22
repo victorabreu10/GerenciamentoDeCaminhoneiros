d917152ef64f5d12852620ce7c3725c517dfb297
VICTOR SANTOS DE ABREU <79884947+victorabreu10@users.noreply.github.com>
2021-11-22 16:45:03 -0300

    Add files via upload

---
+++ GerenciamentoCaminhoneiros/Controllers/CadastroController.cs
@@ -0,0 +1,63 @@
+﻿using CadastroCaminhoneiro.Models;
+using CadastroCaminhoneiro.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CadastroCaminhoneiro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        private readonly ICadastroRepository _cadastroRepository;
+
+        public CadastroController(ICadastroRepository cadastroRepository)
+        {
+            _cadastroRepository = cadastroRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CadastroModel>> BuscarMotorista()
+        {
+            return await _cadastroRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CadastroModel>> BuscarMotoristaPorId(int id)
+        {
+            return await _cadastroRepository.Get(id); 
+        }
+
+        [HttpPost] 
+        public async Task<ActionResult<CadastroModel>>CriarMotorista([FromBody] CadastroModel motorista)
+        {
+            var criarbook = await _cadastroRepository.Create(motorista);
+            return CreatedAtAction(nameof(BuscarMotoristaPorId), new { id = criarbook.Id }, criarbook);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> AtualizarMotorista(int id, [FromBody] CadastroModel motorista)
+        {
+            if(id != motorista.Id)
+            {
+                return BadRequest();
+            }
+
+            await _cadastroRepository.Update(motorista);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeletarMotorista (int id)
+        {
+            var motoristaToDelete = await _cadastroRepository.Get(id);
+            if (motoristaToDelete == null)
+                return NotFound();
+
+            await _cadastroRepository.Delete(motoristaToDelete.Id);
+            return NoContent();
+        }
+    }
+}
---
+++ GerenciamentoCaminhoneiros/Models/CadastroModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroCaminhoneiro.Models
+{
+    public class CadastroModel
+    {
+        public int Id { get; set; }
+        public string nome { get; set; }
+        public string UltimoNome { get; set; }
+        public string MarcaCaminhao { get; set; }
+        public string ModeloCaminhao { get; set; }
+        public string PlacaCaminhao { get; set; }
+        public string eixos { get; set; }
+        public string endereco { get; set; }
+    }
+}
